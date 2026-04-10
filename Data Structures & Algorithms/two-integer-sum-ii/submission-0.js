e1acfbd8971eda9c58dcd3c12ad79aee0df6e582
Sai Dhakshin <67236724+SaiDhakshin@users.noreply.github.com>
2026-04-10 07:37:54 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let l = 0;
+        let r = numbers.length - 1;
+
+        while(l < r){
+            let s = numbers[l] + numbers[r];
+
+            if(s == target){
+                return [l + 1 , r + 1];
+            } else if(s > target){
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return [-1,-1]
+    }
+}
